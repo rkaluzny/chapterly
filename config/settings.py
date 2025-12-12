b4a639d7854fc4c6bd2e2584213edca05d23f1c9
rkaluzny <robin.jerome.kaluzny@gmail.com>
2025-12-12 13:10:32 +0100

    Correctly secure SECRET_KEY and finalize deployment settings

--- config/settings.py
+++ config/settings.py
@@ -12,10 +12,15 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 import os
 from pathlib import Path
 
-# ... (rest of the file until SECRET_KEY)
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-$^n6&zwtooyo_@ewu$mbm-=+bm0ca20zciv*9^&$k*m*1i09pe')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
